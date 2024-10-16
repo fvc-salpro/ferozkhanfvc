0c241172e561a81ede633aff22ab2ffcd10ab716
Muhammad Abbas <i-muhammadabbas@two.cx>
2024-10-16 20:50:54 +0500

    fixes

--- src/slices/BookConsultation/index.tsx
+++ src/slices/BookConsultation/index.tsx
@@ -26,16 +26,18 @@ const BookConsultation = ({ slice }: BookConsultationProps): JSX.Element => {
         <div className="flex flex-col justify-center items-center">
           <div className="flex flex-col justify-center items-center text-center max-w-[950px]">
             <PrismicRichText field={slice.primary.heading} />
-            <div className="max-w-full md:max-w-[580px]">
+            <div className="max-w-full md:max-w-[580px] text-balance mt-[-12px]">
               <PrismicRichText field={slice.primary.text} />
             </div>
           </div>
         </div>
-        <InlineWidget url="https://calendly.com/ferozvisaconsultancy"
+        <InlineWidget
+          url="https://calendly.com/ferozvisaconsultancy"
           iframeTitle="Book Consultation"
-          key={'book-consultation-form'}
+          key={"book-consultation-form"}
           styles={{
-            height: '1000px'
+            height: "1000px",
+            marginTop: "30px",
           }}
         />
       </Bounded>
